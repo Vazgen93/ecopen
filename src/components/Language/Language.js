2fc91d199ab8bdc29729eefa31de6d58fa2a73fd
Vazgen93 <vbagratunyan93@gmail.com>
2020-07-15 02:02:57 +0400

    new2

---
+++ src/components/Language/Language.js
@@ -0,0 +1,29 @@
+export default {
+    AM: {
+        HeaderNav: {
+            home: "Գլխավոր",
+            about: "մեր մասին",
+            labrary: "տեսականի",
+            workers: "գործընկերներ",
+            order: "պատվիրել"
+        }
+    },
+    EN: {
+        HeaderNav: {
+            home: "home",
+            about: "about",
+            labrary: "larary",
+            workers: "workers",
+            order: "order"
+        }
+    },
+    RU: {
+        HeaderNav: {
+            home: "Главная",
+            about: "О-нас",
+            labrary: "Осортимент",
+            workers: "Сотрудник",
+            order: "Заказать"
+        }
+    }
+}
